79f0c962534a88414086f0ae1ab6ac07501a4378
Muhammad Bilal Jawwad <bilal.jawwad@getmyboat.com>
2023-05-18 20:54:37 +0500

    added required dependencies, dev dependencies, tsconfig, nodemon.json

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import type { Express } from "express";
+
+
+dotenv.config();
+const app: Express = express();
+const port = process.env.PORT || 9000;
+
+/**
+ * Health check endpoint
+ */
+app.get("/healthcheck", (req: Request, res: Response) => {
+  res.send("Health check. Server is up and running");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
